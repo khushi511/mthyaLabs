12c10c7d324d7f51e6e9bb2781429cc48fb3481d
khushi511 <khushi.singh5110@gmail.com>
2019-10-30 18:43:17 +0530

    legit related code added

--- server.js
+++ server.js
@@ -1,67 +1,79 @@
 var express = require('express');
+
 var app = express();
-var mysql = require('mysql'); 
+//var mysql = require('mysql'); 
 var bodyParser = require('body-parser');
+const legit = require('legit');
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); 
 //set views folder as entry point for html files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-var connection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'mthyalabtest'
-});
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '',
+// 	database: 'mthyalabtest'
+// });
 
-connection.connect(function(err){
-	if(err){
-		console.log("Error");
-	}
-	else{
-		console.log('Database Connected');
-	}
-})
+// connection.connect(function(err){
+// 	if(err){
+// 		console.log("Error");
+// 	}
+// 	else{
+// 		console.log('Database Connected');
+// 	}
+// })
 
 app.get('/', function(req, res){
+	console.log("LOgin");
 	res.render('login');
 })
 //Api to fetch user activity details 
 app.get('/recommendation?*', function(req, res){
 	var data;
 	console.log("req query", req.query);
-	connection.query("SELECT * FROM user where email = '"+req.query.email+"'", function(error, rows, fields){
-					if(!!error){
-						console.log("error in the query");
-						res.send("SOME error occured while fetching user Activities")
-					}
-					else{
-						data=rows;
-						console.log("succesfull fetched user activity details");
-						res.json(data);
+	// connection.query("SELECT * FROM user where email = '"+req.query.email+"'", function(error, rows, fields){
+	// 				if(!!error){
+	// 					console.log("error in the query");
+	// 					res.send("SOME error occured while fetching user Activities")
+	// 				}
+	// 				else{
+	// 					data=rows;
+	// 					console.log("succesfull fetched user activity details");
+	// 					res.json(data);
 
-					}
-				})
+	// 				}
+	// 			})
 })
 //hone page
 app.post('/home', function(req, res){
-	//search if user exits
-	connection.query("SELECT * FROM user where email = '"+req.body.email+"'", function(error, rows, fields){
-		if(!!error){
-			console.log("error in the query");
-		}
-		else{
-			if(rows.length > 0){
-				console.log("User found in databse, then redirecting to home page");
-				res.render('home');
-			}
-			else{
-				console.log("User doesnot exit");
-				res.send("No such email id");
-			}
-		}
+	legit(req.body.email || 'info@theextraaedge.com')
+	.then(result => {
+		result.isValid ? console.log('Valid!') : console.log('Invalid!');
+		console.log(JSON.stringify(result));
+		res.send("data====="+JSON.stringify(result));
 	})
+	.catch(err => {console.log(err); res.send(JSON.stringify(err));});
+	
+	//search if user exits
+	// connection.query("SELECT * FROM user where email = '"+req.body.email+"'", function(error, rows, fields){
+	// 	if(!!error){
+	// 		console.log("error in the query");
+	// 	}
+	// 	else{
+	// 		if(rows.length > 0){
+	// 			console.log("User found in databse, then redirecting to home page");
+	// 			res.render('home');
+	// 		}
+	// 		else{
+	// 			console.log("User doesnot exit");
+	// 			res.send("No such email id");
+	// 		}
+	// 	}
+	// })
 })
 
 app.listen(1380);
